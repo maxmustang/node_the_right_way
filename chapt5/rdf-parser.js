76f0e9f877306a364618354fdbdcc19a53784757
Max Mustang <max.mustang@icloud.com>
2015-11-04 00:00:44 -0200

    adicao de parser xml pra json

---
+++ chapt5/rdf-parser.js
@@ -0,0 +1,23 @@
+'use strict';
+const
+  fs = require('fs'),
+  cheerio = require('cheerio');
+
+module.exports = function(filename, callback) {
+  fs.readFile(filename, function(err, data){
+    if (err) { return callback(err); }
+    let
+      $ = cheerio.load(data.toString()),
+      collect = function(index, elem) {
+        return $(elem).text();
+      };
+      callback(null, {
+        _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
+        title: $('dcterms\\:title').text(),
+        authors: $('pgterms\\:agent pgterms\\:name').map(collect),
+        subjects: $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect)
+      });
+  });
+};
+//run it
+//node --harmony -e 'require("./rdf-parser.js")("databases/cache/epub/132/pg132.rdf", console.log)'
